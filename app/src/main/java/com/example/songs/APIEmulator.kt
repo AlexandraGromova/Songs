eb9406db289717a936916cf579bc88772ded47e9
sasha <sasadanilina2@gmail.com>
2021-08-18 11:30:39 +0500

    init

---
+++ app/src/main/java/com/example/songs/APIEmulator.kt
@@ -0,0 +1,5 @@
+package com.example.songs
+
+class APIEmulator {
+
+}
---
+++ app/src/main/java/com/example/songs/Song.kt
@@ -0,0 +1,8 @@
+package com.example.songs
+
+
+data class Song(
+    val position: Int,
+    val name: String,
+    val item: String
+)
