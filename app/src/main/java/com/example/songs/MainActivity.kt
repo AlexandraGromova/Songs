87ff532b3da94918f79e9ef95e431525b96584a3
sasha <sasadanilina2@gmail.com>
2021-08-23 19:46:24 +0500

    сделала viewModel

--- app/src/main/java/com/example/songs/MainActivity.kt
+++ app/src/main/java/com/example/songs/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.songs
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 
+const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        val model: ViewModelsSongs by viewModels()
+        model.songs.observe(this,{ Log.d(TAG, it.toString()) } )
     }
 }
--- app/src/main/java/com/example/songs/Repository.kt
+++ app/src/main/java/com/example/songs/Repository.kt
@@ -2,6 +2,4 @@ package com.example.songs
 
 class Repository(private val apiEmulator: APIEmulator) {
     fun getSongList(): List<Song> = apiEmulator.listOfSongs
-
-
 }
